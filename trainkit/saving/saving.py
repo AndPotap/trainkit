cae56a08cc3f970d598b4dc8e8a7685d330fa7a8
AndPotap <apotapczynski@gmail.com>
2023-11-23 13:00:08 -0500

    Added random separation

--- trainkit/saving/saving.py
+++ trainkit/saving/saving.py
@@ -9,7 +9,7 @@ def append_timestamp(filepath, add_random_suffix=False):
     now = datetime.datetime.now()
     timestamp = now.strftime("%Y-%m-%d_%H%M%S")
     if add_random_suffix:
-        timestamp += f"{randint(1, 1_000)}"
+        timestamp += f"_r{randint(1, 1_000)}"
     filepath = Path(filepath)
     aux = f"{filepath.stem}_{timestamp}{filepath.suffix}"
     new_filepath = filepath.with_name(aux)
