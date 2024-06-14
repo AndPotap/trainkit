661cc778647c84482f66294fd44b4304cfd7ac43
Andres Potapczynski <andpotap@amazon.com>
2024-06-14 15:51:24 +0000

    Added saving utilities for json

--- trainkit/saving/saving.py
+++ trainkit/saving/saving.py
@@ -1,9 +1,11 @@
 import datetime
-from pathlib import Path
+import json
 import pickle
 import re
-import yaml
+from pathlib import Path
 from random import randint
+
+import yaml
 
 
 def append_timestamp(filepath, add_random_suffix=False):
@@ -27,24 +29,36 @@ def ask_save_output():
     return save_output
 
 
+def load_json(filepath):
+    with open(file=filepath, mode="r") as file:
+        obj = json.load(file)
+    return obj
+
+
+def save_json(obj, filepath):
+    with open(file=filepath, mode="w") as file:
+        obj = json.dump(obj, file)
+    return obj
+
+
 def load_yml(filepath):
-    with open(file=filepath, mode='r') as file:
+    with open(file=filepath, mode="r") as file:
         obj = yaml.safe_load(file)
     return obj
 
 
 def save_yml(obj, filepath):
-    with open(file=filepath, mode='w') as file:
+    with open(file=filepath, mode="w") as file:
         yaml.dump(obj, file)
 
 
 def load_object(filepath):
-    with open(file=filepath, mode='rb') as f:
+    with open(file=filepath, mode="rb") as f:
         obj = pickle.load(file=f)
     return obj
 
 
 def save_object(obj, filepath, use_highest=True):
     protocol = pickle.HIGHEST_PROTOCOL if use_highest else pickle.DEFAULT_PROTOCOL
-    with open(file=filepath, mode='wb') as f:
+    with open(file=filepath, mode="wb") as f:
         pickle.dump(obj=obj, file=f, protocol=protocol)
