1ed854cc16fcb59e1080e7b76ad9334cae148aa9
Andres Potapczynski <andpotap@amazon.com>
2024-06-14 15:52:47 +0000

    Added new saving json fns to __init__.py

--- trainkit/saving/__init__.py
+++ trainkit/saving/__init__.py
@@ -4,12 +4,16 @@ from trainkit.saving.saving import ask_save_output
 from trainkit.saving.saving import append_timestamp
 from trainkit.saving.saving import load_yml
 from trainkit.saving.saving import save_yml
+from trainkit.saving.saving import load_json
+from trainkit.saving.saving import save_json
 
 __all__ = [
     "load_object",
     "save_object",
     "load_yml",
     "save_yml",
+    "load_json",
+    "save_json",
     "ask_save_output",
     "append_timestamp",
 ]
